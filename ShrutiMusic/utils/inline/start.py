79e531d469c8c9c8e9fa5da673d0bea8daba0e21
ᯓ‌𓆰𝅃 𝚬 ᰻⃪᱂ ꪗ ꪎ 𝁘 🇷🇺 𓆪ꪾ⇢ <heyyyynitin@gmail.com>
2026-03-12 10:41:37 +0530

    Add files via upload

--- ShrutiMusic/utils/inline/start.py
+++ ShrutiMusic/utils/inline/start.py
@@ -1,24 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
 from pyrogram.types import InlineKeyboardButton
 import config
 from ShrutiMusic import app
@@ -27,15 +6,13 @@ def start_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_11"], callback_data="about_page"),
-        ],
-        [
-         
+                text=_["S_B_1"], 
+                url=f"https://t.me/{app.username}?startgroup=true"
+            )
         ],
     ]
     return buttons
+
 
 def private_panel(_):
     buttons = [
@@ -47,26 +24,23 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(
-                text=_["S_B_11"],
-                callback_data="about_page"
-            ),
-            InlineKeyboardButton(
-                text=_["S_B_12"],
-                callback_data="owner_page"
-            )
-        ],
-        [
-            InlineKeyboardButton(
                 text=_["E_X_1"],
                 callback_data="fork_repo"
             ),
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(
+                text=_["S_B_5"], 
+                user_id=config.OWNER_ID
+            ),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="help_page_1")
+            InlineKeyboardButton(
+                text=_["S_B_4"], 
+                callback_data="help_page_1"
+            )
         ],
     ]
     return buttons
+
 
 def about_panel(_):
     buttons = [
@@ -75,10 +49,14 @@ def about_panel(_):
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_GROUP),
         ],
         [
-            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper")
+            InlineKeyboardButton(
+                text=_["BACK_BUTTON"], 
+                callback_data="settingsback_helper"
+            )
         ]
     ]
     return buttons
+
 
 def owner_panel(_):
     buttons = [
@@ -91,19 +69,10 @@ def owner_panel(_):
             InlineKeyboardButton(text=_["S_H_4"], url=config.DONATE),
         ],
         [
-            InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper")
+            InlineKeyboardButton(
+                text=_["BACK_BUTTON"], 
+                callback_data="settingsback_helper"
+            )
         ]
     ]
     return buttons
-
-
-# ©️ Copyright Reserved - @NoxxOP  Nand Yaduwanshi
-
-# ===========================================
-# ©️ 2025 Nand Yaduwanshi (aka @NoxxOP)
-# 🔗 GitHub : https://github.com/NoxxOP/ShrutiMusic
-# 📢 Telegram Channel : https://t.me/ShrutiBots
-# ===========================================
-
-
-# ❤️ Love From ShrutiBots 
